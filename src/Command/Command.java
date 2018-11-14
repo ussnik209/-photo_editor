68b48893f85ff4526f522dddb5103498ada2b30e
Vadim Tatarnikov <vadimtatarnikov98@gmail.com>
2018-11-14 19:42:23 +0300

    Update Command class

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -1,4 +1,19 @@
 package Command;
 
-public class Command {
+import Image.PixelImage;
+import Strategy.Filter;
+
+public abstract class Command {
+
+    protected Filter filt;
+
+    public Filter getFilt() {
+        return filt;
+    }
+
+    public Command (Filter f) {
+        this.filt = f;
+    }
+
+    public abstract void execute(PixelImage img);
 }
