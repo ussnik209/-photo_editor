58a47ceb0c441ca8da5f616352c9cce81215d35c
Vadim Tatarnikov <36814223+B1nvoker@users.noreply.github.com>
2018-12-05 11:55:57 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import UI.GUI;
 
 public class Main {
-    public static void main(String[] args) throws CloneNotSupportedException{
+    public static void main(String[] args) throws CloneNotSupportedException {
         final GUI gui = new GUI();
         gui.start();
     }
